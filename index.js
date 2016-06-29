90848bdce48150cafb9c23a57e1a44dea95a7194
Martin Möhnen <martin@moehnen.de>
2016-06-29 09:24:05 +0200

    cleanup

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-const getPlayer = (panel) => panel.getElementsByTagName("audio")[0]
+const getPlayer = panel => panel.getElementsByTagName("audio")[0]
 
-const getPanel = (player) => player.parentElement.parentElement;
+const getPanel = (player) => player.parentElement.parentElement
 
-const isPlaying = (panel) => !getPlayer(panel).paused
+const isPlaying = panel => !getPlayer(panel).paused
 
-const startPlaying = (panel) => getPlayer(panel).play()
+const startPlaying = panel => getPlayer(panel).play()
 
-const stopPlaying = (panel) => getPlayer(panel).pause();
+const stopPlaying = panel => getPlayer(panel).pause()
 
-const togglePlaying = (panel) => isPlaying(panel) ? stopPlaying(panel) : startPlaying(panel)
+const togglePlaying = panel => isPlaying(panel) ? stopPlaying(panel) : startPlaying(panel)
 
-const panelClick = (panel) => togglePlaying(panel)
+const panelTemplate = document.getElementById("panel").innerHTML
 
-const source = document.getElementById("paneltemplate").innerHTML
+const panelHtml = panel => panelTemplate.replace(/{(.+?)}/mg, (match, key) => panel[key])
 
-const panelHtml = (panel) => source.replace(/{(.+?)}/mg, (match, key) => panel[key])
-
-const rowHtml = (row) => row.map(panel => panelHtml(panel)).join('')
+const rowHtml = row => row.map(panel => panelHtml(panel)).join('')
 
 const appHtml = sounds.map(row => `<div class="row">${rowHtml(row)}</div>`).join('')
 
 document.body.insertAdjacentHTML("afterbegin", appHtml)
 
-document.addEventListener("playing", (e) => getPanel(e.target).classList.add('active'), true);
+document.addEventListener("playing", e => getPanel(e.target).classList.add('active'), true)
 
-document.addEventListener("pause", (e) => getPanel(e.target).classList.remove('active'), true);
+document.addEventListener("pause", e => getPanel(e.target).classList.remove('active'), true)
 
-document.onkeydown = (e) => panelClick(document.getElementById(String.fromCharCode(e.keyCode)))
+document.onkeydown = e => togglePlaying(document.getElementById(String.fromCharCode(e.keyCode)))
